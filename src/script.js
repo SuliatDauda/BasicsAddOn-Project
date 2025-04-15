a3d767c4d8d37d942f8402abacf35a9f6471e678
SuliatDauda <suliatdauda1@gmail.com>
2025-04-15 11:14:14 +0100

    Prompt
    Added functions and prompt to alert question when you click on "DO you want to try Nigerian food" button

--- src/script.js
+++ src/script.js
@@ -5,3 +5,19 @@ function changeTheme() {
 
 let themeButton = document.querySelector(".theme-button");
 themeButton.addEventListener("click", changeTheme);
+
+function prompt() {
+  let name = prompt("What is your Name?");
+  alert("Welcome" + " " + name + "!");
+  let nationality = prompt("What is your Nationality");
+  if (nationality === "Nigerian" || "Nigeria") {
+    alert("My Person Welcome!");
+  } else {
+    alert("We are Thrilled to have you🥰");
+  }
+  let food = prompt("Which Nigerian food do you want to try?");
+  alert("Place your Order!");
+}
+
+let otherButton = document.querySelector("#otherButton");
+otherButton.addEventListener("click", prompt);
