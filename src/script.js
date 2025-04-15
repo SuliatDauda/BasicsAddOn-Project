8ad1750cd9f12dde8af5a2e69f6ed9cc1448fe2e
SuliatDauda <suliatdauda1@gmail.com>
2025-04-15 10:44:32 +0100

    Dark Theme js
    Added js function to toggle from light theme to dark theme. added eventListener and classLIst plus toggle.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
-html;
+function changeTheme() {
+  let body = document.querySelector("body");
+  body.classList.toggle("dark");
+}
+
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", changeTheme);
